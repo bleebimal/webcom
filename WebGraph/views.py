5f066350939d2b5d1dfbca98b3483a29d6a1a1c9
Bimal Gaire <bgaire@alumni.deerwalk.edu.np>
2019-07-18 21:55:48 +0545

    value error resolve

--- WebGraph/views.py
+++ WebGraph/views.py
@@ -43,40 +43,56 @@ def analyze_data(request):
     except:
         messages.add_message(request, messages.ERROR, 'Some Error Occurred!!!')
 
-    combination, ipc_r, ipc_m, cpc_r, cpc_m, upc_r, upc_m = analyze(full_filename)
+    combination, ipc_r, ipc_p, cpc_p, upc_p, ipc_m, cpc_r, cpc_m, upc_r, upc_m = (analyze(full_filename) for i in range(10))
+
     print("VIEWS.PY " + str(combination))
 
     if combination is not None:
         classes = ["table", "table-bordered", "table-striped", "table-hover", "table-dark"]
 
         ipc_r['recall'] = ipc_r['recall'].astype(float)
+        ipc_p['precision'] = ipc_p['precision'].astype(float)
         ipc_m['mpr'] = ipc_m['mpr'].astype(float)
         cpc_r['recall'] = cpc_r['recall'].astype(float)
+        cpc_p['precision'] = cpc_p['precision'].astype(float)
         cpc_m['mpr'] = cpc_m['mpr'].astype(float)
         upc_r['recall'] = upc_r['recall'].astype(float)
+        upc_p['precision'] = upc_p['precision'].astype(float)
         upc_m['mpr'] = upc_m['mpr'].astype(float)
 
         ipc_r_list = ipc_r.values.tolist()
         ipc_r_list.insert(0, ["IPC", "Recall"])
+        ipc_p_list = ipc_r.values.tolist()
+        ipc_p_list.insert(0, ["IPC", "PRECISION"])
         ipc_m_list = ipc_m.values.tolist()
         ipc_m_list.insert(0, ["IPC", "MPR"])
         cpc_r_list = cpc_r.values.tolist()
         cpc_r_list.insert(0, ["CPC", "Recall"])
+        cpc_p_list = cpc_r.values.tolist()
+        cpc_p_list.insert(0, ["CPC", "PRECISION"])
         cpc_m_list = cpc_m.values.tolist()
         cpc_m_list.insert(0, ["CPC", "MPR"])
         upc_r_list = upc_r.values.tolist()
         upc_r_list.insert(0, ["UPC", "Recall"])
+        upc_p_list = upc_r.values.tolist()
+        upc_p_list.insert(0, ["UPC", "PRECISION"])
         upc_m_list = upc_m.values.tolist()
         upc_m_list.insert(0, ["UPC", "MPR"])
 
         ipc_r_data = SimpleDataSource(data=ipc_r_list)
+        ipc_p_data = SimpleDataSource(data=ipc_p_list)
         ipc_m_data = SimpleDataSource(data=ipc_m_list)
         cpc_r_data = SimpleDataSource(data=cpc_r_list)
+        cpc_p_data = SimpleDataSource(data=cpc_p_list)
         cpc_m_data = SimpleDataSource(data=cpc_m_list)
         upc_r_data = SimpleDataSource(data=upc_r_list)
+        upc_p_data = SimpleDataSource(data=upc_p_list)
         upc_m_data = SimpleDataSource(data=upc_m_list)
 
         ipc_chart_r = ColumnChart(ipc_r_data, options={'title': 'IPC Recall', 'width': 800, 'height': 500,
+                                                       'hAxis': {'slantedText': 'true', 'slantedTextAngle': 55,
+                                                                 'showTextEvery': 1}, })
+        ipc_chart_p = ColumnChart(ipc_p_data, options={'title': 'IPC Precision', 'width': 800, 'height': 500,
                                                        'hAxis': {'slantedText': 'true', 'slantedTextAngle': 55,
                                                                  'showTextEvery': 1}, })
         ipc_chart_m = ColumnChart(ipc_m_data, options={'title': 'IPC MPR', 'width': 800, 'height': 500,
@@ -85,10 +101,16 @@ def analyze_data(request):
         cpc_chart_r = ColumnChart(cpc_r_data, options={'title': 'CPC Recall', 'width': 800, 'height': 520,
                                                        'hAxis': {'slantedText': 'true', 'slantedTextAngle': 55,
                                                                  'showTextEvery': 1}, })
+        cpc_chart_p = ColumnChart(cpc_p_data, options={'title': 'CPC PRECISION', 'width': 800, 'height': 520,
+                                                       'hAxis': {'slantedText': 'true', 'slantedTextAngle': 55,
+                                                                 'showTextEvery': 1}, })
         cpc_chart_m = ColumnChart(cpc_m_data, options={'title': 'CPC MPR', 'width': 800, 'height': 520,
                                                        'hAxis': {'slantedText': 'true', 'slantedTextAngle': 55,
                                                                  'showTextEvery': 1}, })
         upc_chart_r = ColumnChart(upc_r_data, options={'title': 'UPC Recall', 'width': 800, 'height': 500,
+                                                       'hAxis': {'slantedText': 'true', 'slantedTextAngle': 55,
+                                                                 'showTextEvery': 1}, })
+        upc_chart_p = ColumnChart(upc_p_data, options={'title': 'UPC PRECISION', 'width': 800, 'height': 500,
                                                        'hAxis': {'slantedText': 'true', 'slantedTextAngle': 55,
                                                                  'showTextEvery': 1}, })
         upc_chart_m = ColumnChart(upc_m_data, options={'title': 'UPC MPR', 'width': 800, 'height': 500,
@@ -96,10 +118,13 @@ def analyze_data(request):
                                                                  'showTextEvery': 1}, })
 
         ipc_table_r = ipc_r.to_html(index=False, classes=classes)
+        ipc_table_p = ipc_p.to_html(index=False, classes=classes)
         ipc_table_m = ipc_m.to_html(index=False, classes=classes)
         cpc_table_r = cpc_r.to_html(index=False, classes=classes)
+        cpc_table_p = cpc_p.to_html(index=False, classes=classes)
         cpc_table_m = cpc_m.to_html(index=False, classes=classes)
         upc_table_r = upc_r.to_html(index=False, classes=classes)
+        upc_table_p = upc_p.to_html(index=False, classes=classes)
         upc_table_m = upc_m.to_html(index=False, classes=classes)
 
         combination = pandas.DataFrame(combination, columns=['IPC and CPC', 'IPC and UPC'])
@@ -107,16 +132,23 @@ def analyze_data(request):
 
         context = {
             'ipc_chart_r': ipc_chart_r,
+            'ipc_chart_p': ipc_chart_p,
             'ipc_chart_m': ipc_chart_m,
             'cpc_chart_r': cpc_chart_r,
+            'cpc_chart_p': cpc_chart_p,
             'cpc_chart_m': cpc_chart_m,
             'upc_chart_r': upc_chart_r,
+            'upc_chart_p': upc_chart_p,
             'upc_chart_m': upc_chart_m,
+
             'ipc_table_r': ipc_table_r,
+            'ipc_table_p': ipc_table_p,
             'ipc_table_m': ipc_table_m,
             'cpc_table_r': cpc_table_r,
+            'cpc_table_p': cpc_table_p,
             'cpc_table_m': cpc_table_m,
             'upc_table_r': upc_table_r,
+            'upc_table_p': upc_table_p,
             'upc_table_m': upc_table_m,
             'combination': combination
         }
